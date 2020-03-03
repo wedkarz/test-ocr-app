59f419f73669718eb347b857b351ac394ceb4bca
Artur Rybak <artur.rybak@jeppesen.com>
2020-03-03 22:04:49 +0100

    Initial commit. Removed SceneDelegate for backward compatibility with ios 12

---
+++ test-ocr-app/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  test-ocr-app
+//
+//  Created by Artur Rybak on 03/03/2020.
+//  Copyright © 2020 IT ART. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        
+        return true
+    }
+}
+
